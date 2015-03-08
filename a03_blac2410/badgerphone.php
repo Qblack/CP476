eb20b6c2b7c27d642388b4a2ed941a1290a1b7c7
Qblack <blac2410@mylaurier.ca>
2015-03-08 14:19:34 -0400

    Banana phone

---
+++ a03_blac2410/badgerphone.php
@@ -0,0 +1,12 @@
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="jumbotron center"><h1>Welcome to my mobile site</h1></div>
+                <iframe style="align-content: center" width="640" height="390" src="https://www.youtube.com/embed/wky5H1xC6-I" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</body>
+
+
